3a1f3238947848ab7be13b5219abefd70adc95d6
Ryan Winstead <rwinstead6@gatech.edu>
2020-12-27 21:00:34 -0600

    initial scene

---
+++ JoshJamGame/Assets/movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 20f;
+
+    //Jump declarations
+    public Vector3 jump;
+    public float jumpForce = 20.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+
+        }
+
+
+
+        transform.position = pos;
+    }
+}
+
+
+
+
